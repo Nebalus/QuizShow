96f1e842825e0f57b6bc6c5fb861d6b104561b17
Nebalus <56486387+Nebalus@users.noreply.github.com>
2022-06-03 00:05:15 +0200

    Add files via upload

--- SRC.ino
+++ SRC.ino
@@ -2,15 +2,14 @@
 #include "Quizshow_Helper.h"
 
 const int buttonamount = 6;
-//INDEX: {ButtonPin, }
-const int buttonpins[buttonamount] = 
-{
-  12, 
-  11,
-  10,
-  9,
-  8,
-  7
+//INDEX: {ButtonPin, Pressduration, lastpressupdate}
+long buttonpins[buttonamount][3] = {
+  {12, 0, 0}, 
+  {11, 0, 0},
+  {10, 0, 0},
+  {9, 0, 0},
+  {8, 0, 0},
+  {7, 0, 0}
 };
 
 const int playerbuttonamount = 4;
@@ -20,13 +19,15 @@ const int playerbuttons[playerbuttonamount][2] =
   {0, 0},
   {1, 1},
   {2, 2},
-  {3, 3},
+  {3, 3}
 };
 
-int registeredplayers[playerbuttonamount][3] = 
+int registeredplayers[playerbuttonamount][2] = 
 {
   
 };
+
+
 
 //############################################################
 
@@ -48,13 +49,35 @@ void loop()
 {
   for (int i = 0; i < buttonamount; i++)
   {
-    const buttonpinid = buttonpins[i][0];
+    const long buttonpinid = buttonpins[i][0];
     if (digitalRead(buttonpinid) == HIGH)
     {
-      
-      buzz(1000);
+      const long timepressed = millis() - buttonpins[i][1];
+      if(timepressed > 0)
+      {
+        const long lastpressupdate = timepressed - (millis() - buttonpins[i][2]);
+        if(lastpressupdate <= 1000 && timepressed >= 1000)
+        {
+          //Wird ein mal ausgeführt wenn der knopf 1 Sekunde dedrückt wurde
+          buzz(1000);
+        }
+        
+        Serial.println(timepressed);
+        //Wird wieder und wieder ausgefühlt wenn der knopf gedrückt wurde
+      }
+      else
+      {
+        //Wird ein mal ausgeführt wenn der knopf dedrückt wurde
+      }
+
+      buttonpins[i][2] = millis();
+    }
+    else
+    {
+      buttonpins[i][1] = millis();
     }
   }
 
   tick();
+  delay(20);
 }
